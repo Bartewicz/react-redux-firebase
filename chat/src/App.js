2a2aaccd2914dc8d92c1fb239555c33c560a30bd
Bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-05-21 10:05:18 +0200

    Add drawer

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Drawer from './common/Drawer'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <Drawer />
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -14,8 +18,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
